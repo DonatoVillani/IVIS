67dade0e68ee28e77fef0748034341af636cc163
Donato Villani <donato.villani@students.fhnw.ch>
2018-05-24 22:51:28 +0200

    Anpassung von typen auf var / Anpassen von for loops / Anpassen von functions

--- P5js_Klangschalen_DNA/sketch.js
+++ P5js_Klangschalen_DNA/sketch.js
@@ -1,67 +1,189 @@
-/**
- * Created by Marco on 17.04.18.
- */
-//Minim       minim;
-//AudioPlayer in;
-//FFT         fft;
+var     minim;
+var ini;
+var       fft;
 
-// ====================CONSTANTS======================
-var canvasHeight = 800;
-var canvasWidth = 1000;
+var smoothing = 0.5;
+var fftReal;
+var fftImag; // float [] fftImag
+var fftSmooth; // float [] fftSmooth
+var specSize;
+var scaledAmplitude;
+var amps; // float [] amps
 
-const ampFactor = 0.8;
-// ===================================================
+var sampleRate= 44100;//sapleRate of 44100
+var max= [sampleRate/2];//array that contains the half of the sampleRate size, because FFT only reads the half of the sampleRate frequency. This array will be filled with amplitude values.
+var maximum;//the maximum amplitude of the max array
+var frequency;//the frequency in hertz
 
+var mandali; // font check how to do it in JS!!
 
-var amps = [];
-
-
-function preload() {
-    soundFormats('mp3', 'ogg');
-    mySound = loadSound('assets/klangschale_gr7_sc37307.mp3');
+function setup(){
+    createCanvas(4096, 3072); //P3D);
+  var minim = new Minim;
+ini = minim.loadFile("klangschale_gr7_sc37307.mp3", 2048),
+ini.play(),
+    fft = new FFT(ini.bufferSize(), ini.sampleRate()),
+    specSize = fft.specSize();
+    System.out.println(specSize);
+    fftSmooth = new float[specSize];
+    fftReal   = new float[specSize];
+    amps   = new float[specSize];
+    colorMode(RGB,100);
+    background(10);
+    mandali = createFont("Mandali-Regular.ttf",128);
 }
 
-function setup() {
+function draw(){
+    //background(0);
+    stroke(255);
 
-    var cnv = createCanvas(canvasWidth,canvasHeight);
-    background(0);
-    fft = new p5.FFT();
-    mySound.amp(ampFactor);
-    mySound.play();
-}
+    fft.forward(ini.left);
+    fftReal = fft.getSpectrumReal();
+    fftImag = fft.getSpectrumImaginary();
+    findNote();
+    //System.out.println(frequency);
+    for(var i = 0; i < specSize; i++)
+    {
+        var amp = fft.getBand(i);
+        scaledAmplitude = amp*8; //scale amp according to needs
 
-function draw() {
-
-
-    var spectrum = fft.analyze();
-
-    for (var i = 0; i< spectrum.length; i++){
-        var scaledAmplitude = spectrum[i]*1.5;
-       // var x = map(i, 0, spectrum.length, 0, width);
-       // var h = -height + map(spectrum[i], 0, 255, height, 0);
-        //rect(x, height, width / spectrum.length, h )
-        strokeWeight(1);
+        strokeWeight(5);
         noFill();
+
+        //Inside Yellow, the bigger the more reddish
         stroke(100,100-(scaledAmplitude/4),15,8);
 
-        //ellipse(((canvasWidth/100)*i), canvasHeight/2, scaledAmplitude*(canvasHeight/400), scaledAmplitude*(canvasHeight/400));
-        ellipse(((canvasWidth/100)*i), canvasHeight/2, scaledAmplitude*(canvasHeight/400), scaledAmplitude*(canvasHeight/400));
+        //N0p3
+        //stroke((scaledAmplitude/35)*100,((100-(scaledAmplitude/4))/(scaledAmplitude/35))*100,((scaledAmplitude/35)*15),5);
+
+        //ellipse((width/30)+((width/100)*i), height/2, (width/5)+fftSmooth[i]*(height/400), (width/5)+fftSmooth[i]*(height/400));
+        ellipse(((width/100)*i), height/2, scaledAmplitude*(height/400), scaledAmplitude*(height/400));
+
         //getMaxAmp per Band
-        //if(scaledAmplitude>amps[i]){amps[i]=scaledAmplitude;}
+        if(scaledAmplitude>amps[i]){amps[i]=scaledAmplitude;}
+
 
     }
+    //text("smoothing: " + (int)(smoothing*100),10,10);
+}
+function keyPressed(){
+    var textCounter = 0;
+    var inc = 0.01;
+    //if(keyCode == UP && smoothing < 1-inc) smoothing += inc;
+   // if(keyCode == DOWN && smoothing > inc) smoothing -= inc;
+    if (key == 's') {
+        save("normal.png");
+        saveHiRes(2);
+        exit();
+    }
+    if(key=='p'){
+        var lastBand = 0;
+        var textY = (height/2)-50;
+        textSize(128);
+        textFont(mandali);
+        for(var i =0;i<amps.length;i++){
+            if(amps[i]>200){
+                textCounter +=1;
+                System.out.println(i+" scaledAmp: "+amps[i]+" with Hertz of: "+fft.indexToFreq(i));
+                fill(100,30);
+                textAlign(LEFT,BOTTOM);
+                //text((int)(fft.indexToFreq(i))+" Hz",(width/100)*i,((i-lastBand<5?textY+200:textY)/4)*3);
+                text((int)(fft.indexToFreq(i))+" Hz",(width/8*7),100+(150*textCounter));
+                var midi= 69+12*(log(fft.indexToFreq(i)/440)/log(2));// formula that transform frequency to midi numbers
+                var n= int (midi);//cast to int
+                var note ="";
+                //the octave have 12 tones and semitones. So, if we get a modulo of 12, we get the note names independently of the frequency
+                if (n%12==9)
+                {
+                    note = ("a");
+                }
 
-  /*  var waveform = fft.waveform();
-    noFill();
-    beginShape();
-    stroke(255,0,0); // waveform is red
-    strokeWeight(1);
-    for (var i = 0; i< waveform.length; i++){
-        var x = map(i, 0, waveform.length, 0, width);
-        var y = map( waveform[i], -1, 1, 0, height);
-        vertex(x,y);
-    }*/
+                if (n%12==10)
+                {
+                    note = ("a#");
+                }
 
-    endShape();
+                if (n%12==11)
+                {
+                    note = ("b");
+                }
+
+                if (n%12==0)
+                {
+                    note = ("c");
+                }
+
+                if (n%12==1)
+                {
+                    note = ("c#");
+                }
+
+                if (n%12==2)
+                {
+                    note = ("d");
+                }
+
+                if (n%12==3)
+                {
+                    note = ("d#");
+                }
+
+                if (n%12==4)
+                {
+                    note = ("e");
+                }
+
+                if (n%12==5)
+                {
+                    note = ("f");
+                }
+
+                if (n%12==6)
+                {
+                    note = ("f#");
+                }
+
+                if (n%12==7)
+                {
+                    note = ("g");
+                }
+
+                if (n%12==8)
+                {
+                    note = ("g#");
+                }
+                fill(100,70,0,90);
+                textAlign(CENTER,CENTER);
+                text(note,(width/100)*i,((i-lastBand<5?textY+100:textY)));
+                lastBand = i;
+            }
+        }
+
+        save("image.png");
+    }
 }
 
+function findNote() {
+    var frequency = 0.0;
+    for (var f=0;f<sampleRate/2;f++) { //analyses the amplitude of each frequency analysed, between 0 and 22050 hertz
+        max[f]=fft.getFreq(float(f)); //each index is correspondent to a frequency and contains the amplitude value
+    }
+    maximum=(max);//get the maximum value of the max array in order to find the peak of volume
+
+    for (var i=0; i<max.length; i++) {// read each frequency in order to compare with the peak of volume
+        if (max[i] == maximum) {//if the value is equal to the amplitude of the peak, get the index of the array, which corresponds to the frequency
+            frequency= i;
+        }
+    }
+    return frequency;
+}
+
+
+// function saveHiRes (scaleFactor) {
+//     PGraphics hires = createGraphics(width*scaleFactor, height*scaleFactor, P3D); // CREATION OF 3D PGRAPHICS OBJECT
+//     beginRecord(hires);
+//     hires.scale(scaleFactor);
+//     draw();
+//     endRecord();
+//     hires.save("hires.png");
+// }
--- Processing_Klangschalen_DNA/klangschale_try2.pde
+++ Processing_Klangschalen_DNA/klangschale_try2.pde
@@ -1,6 +1,3 @@
-import ddf.minim.analysis.*;
-import ddf.minim.*;
-
 Minim       minim;
 AudioPlayer in;
 FFT         fft;
@@ -49,19 +46,19 @@ void draw(){
   {
     float amp = fft.getBand(i);
     scaledAmplitude = amp*8; //scale amp according to needs
-    
+
     strokeWeight(5);
     noFill();
-    
+
     //Inside Yellow, the bigger the more reddish
     stroke(100,100-(scaledAmplitude/4),15,8);
-    
+
     //N0p3
     //stroke((scaledAmplitude/35)*100,((100-(scaledAmplitude/4))/(scaledAmplitude/35))*100,((scaledAmplitude/35)*15),5);
-    
+
      //ellipse((width/30)+((width/100)*i), height/2, (width/5)+fftSmooth[i]*(height/400), (width/5)+fftSmooth[i]*(height/400));
      ellipse(((width/100)*i), height/2, scaledAmplitude*(height/400), scaledAmplitude*(height/400));
-  
+
     //getMaxAmp per Band
   if(scaledAmplitude>amps[i]){amps[i]=scaledAmplitude;}
 
@@ -95,62 +92,62 @@ void keyPressed(){
      float midi= 69+12*(log(fft.indexToFreq(i)/440)/log(2));// formula that transform frequency to midi numbers
      int n= int (midi);//cast to int
      String note ="";
-     //the octave have 12 tones and semitones. So, if we get a modulo of 12, we get the note names independently of the frequency  
+     //the octave have 12 tones and semitones. So, if we get a modulo of 12, we get the note names independently of the frequency
      if (n%12==9)
       {
         note = ("a");
       }
-     
+
       if (n%12==10)
       {
         note = ("a#");
       }
-     
+
       if (n%12==11)
       {
         note = ("b");
       }
-     
+
       if (n%12==0)
       {
         note = ("c");
       }
-     
+
       if (n%12==1)
       {
         note = ("c#");
       }
-     
+
       if (n%12==2)
       {
         note = ("d");
       }
-     
+
       if (n%12==3)
       {
         note = ("d#");
       }
-     
+
       if (n%12==4)
       {
         note = ("e");
       }
-     
+
       if (n%12==5)
       {
         note = ("f");
       }
-     
+
       if (n%12==6)
       {
         note = ("f#");
       }
-     
+
       if (n%12==7)
       {
         note = ("g");
       }
-     
+
       if (n%12==8)
       {
         note = ("g#");
@@ -161,7 +158,7 @@ void keyPressed(){
       lastBand = i;
    }
    }
-   
+
    save("image.png");
   }
 }
@@ -169,10 +166,10 @@ void keyPressed(){
 float findNote() {
   float frequency = 0.0;
   for (int f=0;f<sampleRate/2;f++) { //analyses the amplitude of each frequency analysed, between 0 and 22050 hertz
-    max[f]=fft.getFreq(float(f)); //each index is correspondent to a frequency and contains the amplitude value 
+    max[f]=fft.getFreq(float(f)); //each index is correspondent to a frequency and contains the amplitude value
   }
   maximum=max(max);//get the maximum value of the max array in order to find the peak of volume
- 
+
   for (int i=0; i<max.length; i++) {// read each frequency in order to compare with the peak of volume
     if (max[i] == maximum) {//if the value is equal to the amplitude of the peak, get the index of the array, which corresponds to the frequency
       frequency= i;
@@ -181,7 +178,7 @@ float findNote() {
   return frequency;
 }
 
- 
+
 void saveHiRes(int scaleFactor) {
   PGraphics hires = createGraphics(width*scaleFactor, height*scaleFactor, P3D); // CREATION OF 3D PGRAPHICS OBJECT
   beginRecord(hires);
